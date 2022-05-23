41a86e19655ca9515a51eac71820bab234c1a1a5
jvcataquiz <jvcataquiz@gmail.com>
2022-05-23 15:29:07 +0800

    Coding Exercise 4.8 in Programming for Beginners Using Python is Completed

---
+++ Course 1 - Programming for Beginners Using Python/4.8Exercise.py
@@ -0,0 +1,15 @@
+
+#for for loop
+furniture = ["table","chair","cabinet","desk","couch"]
+for x in furniture:
+    if x == "cabinet":
+        continue
+    print(x)
+#this is for while loop
+i=1
+while i < 15:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 15")
+    
