240491eb8fd9b149b917518c0cfdf50f0ae2afe8
jvcataquiz <jvcataquiz@gmail.com>
2022-05-23 13:48:53 +0800

    Coding Exercise 2.7 in Programming for Beginners Using Python is Completed

---
+++ Programming for Beginners Using Python/2.7Exercise.py
@@ -0,0 +1,14 @@
+
+name = input("Enter your name: ")
+age= input("Enter your age: ")
+color= input("Enter your Favorite color: ")
+movie= input("Enter your Favorite Movie: ")
+phone_number= input("Enter your Phone number: ")
+motto = input("Motto in Life:")
+
+print("Name: " + name)
+print("Age: " + age)
+print("Favorite Color: " + color)
+print("Favorite Movie: " + movie)
+print("Phone Number: " + phone_number)
+print("Motto in life: " + motto)
