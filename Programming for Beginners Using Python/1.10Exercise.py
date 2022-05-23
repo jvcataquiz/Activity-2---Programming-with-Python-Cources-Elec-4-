4d8cac03b1cbe9a56663eaec785f6cf4c13f38cd
jvcataquiz <jvcataquiz@gmail.com>
2022-05-23 13:15:06 +0800

    Coding Exercise 1.10 in Programming for Beginners Using Python is Completed

---
+++ Programming for Beginners Using Python/1.10Exercise.py
@@ -0,0 +1,8 @@
+character_job = "pirate"
+character_ride = "ship"
+character_souvenir = "gold"
+character_pet = "parrot"
+print("There was once a " + character_job + " who loved adventure.")
+print("The " + character_job + "would take her " + character_ride + ".")
+print("She brings a lot of " + character_souvenir + ".")
+print("Then she goes home to her pet" + character_pet + ", Chuckles")
